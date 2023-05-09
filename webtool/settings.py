253c935f1f7fee01a1d191eb8dadb30f30438b8c
unknown <md.e.h.ashiq@gmail.com>
2023-05-09 15:19:03 +0600

    update

--- webtool/settings.py
+++ webtool/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = "lkadgjfayoewffesdfhiufphaeiuphfarysssfapiehrfushdakjfdalkfhdjl"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['webtool-ooee.onrender.com', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
